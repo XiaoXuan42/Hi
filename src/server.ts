91cfdc9c2d26407df04bf83ee70daff1fa67184f
DunderBird <522169030@qq.com>
2022-05-25 17:21:04 +0800

    dynamic site to static site

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,28 @@
-import express, { Express } from 'express';
 import { FileTree } from './file';
+import * as http from 'http';
 
 export class Server {
-    server: Express;
-
-    constructor(public filetree: FileTree) {
-        this.server = express();
-    }
+    server: http.Server | undefined;
+    constructor(public filetree: FileTree) {}
 
     start() {
         const port = 8080;
-        this.server.get('*', (req, res) => {
-            const content = this.filetree.get_by_url(req.path);
-            if (content) {
-                res.send(content);
+        this.server = http.createServer((req, res) => {
+            if (req.url) {
+                const content = this.filetree.get_by_url(req.url);
+                if (content) {
+                    res.writeHead(200);
+                    res.end(content);
+                } else {
+                    res.writeHead(404);
+                    res.end(`${req.url} not found.`);
+                }
             } else {
-                res.sendStatus(404);
+                res.writeHead(404);
+                res.end("No url provided.");
             }
         });
-        this.server.listen(port, () => {
-            console.log(`Server is running at https://localhost:${port}`);
-        });
+        console.log(`Server created on http://localhost:${port}`);
+        this.server.listen(port);
     }
 }
